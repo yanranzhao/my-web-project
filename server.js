cdd30cdf9f88d5c25087a7f52d9d1cfe1800738d
Yanranzhao <yanranzhao@yanrans-mbp.wireless.concordia.ca>
2019-05-25 14:20:45 -0400

    Added NodeJs

---
+++ server.js
@@ -0,0 +1,10 @@
+// write a server file for nodejs
+
+// import http package for nodejs
+const http = require('http');
+
+const server = http.createServer( (req, res) => {
+  res.end('This is my first response');
+} );
+
+server.listen(process.env.PORT || 3000);
