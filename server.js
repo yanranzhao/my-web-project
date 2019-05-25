5957996013957af36071ce4c88a6adb88cb0a309
Yanranzhao <yanranzhao@yanrans-mbp.wireless.concordia.ca>
2019-05-25 14:55:49 -0400

    Added error handling and nodemon

--- server.js
+++ server.js
@@ -1,13 +1,58 @@
-// write a server file for nodejs
+const app = require("./backend/app");
+const debug = require("debug")("node-angular");
+const http = require("http");
 
-// import http package for nodejs
-const http = require('http');
-const app = require('./backend/app');
+// ensure is a valid number
+const normalizePort = val => {
+  var port = parseInt(val, 10);
 
-// set up express and connect
-const port = process.env.PORT || 3000;
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
 
-app.set('port',port);
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+};
+
+// Added error handling
+const onError = error => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const bind = typeof port === "string" ? "pipe " + port : "port " + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+// is listening incoming request
+const onListening = () => {
+  const addr = server.address();
+  const bind = typeof port === "string" ? "pipe " + port : "port " + port;
+  debug("Listening on " + bind);
+};
+
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+
 const server = http.createServer(app);
+server.on("error", onError);          // if wrong go to error
+server.on("listening", onListening);  // if smooth go
+server.listen(port);                  // start the server
 
-server.listen(port);
+// And add nodemon "npm install --save-dev nodemon"
+// Add script in package.json and run "npm run start:server"
